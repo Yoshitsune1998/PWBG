bd854d0ecc4b7568b0840e96188132f5b2aff2e1
Abdul Yasir Halomoan <smpyasir01@gmail.com>
2018-07-17 22:54:00 +0700

    asda

---
+++ PWBG_BOT/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PWBG_BOT
+{
+    class Config
+    {
+        static Config()
+        {
+
+        }
+    }
+
+    public struct BotConfig
+    {
+        public string token;
+        public string cmdPrefix;
+    }
+
+}
